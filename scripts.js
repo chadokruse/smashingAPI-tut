c177314f72d1afa9817bf58be1d0d62c1bb7a4af
Chad Kruse <chad@10speak.com>
2013-01-08 16:29:03 -0600

    Use Short Form getJSON call + misc javascript cleanup

--- scripts.js
+++ scripts.js
@@ -14,31 +14,22 @@ $(document).ready(function() {
 			//Grab the movie title and store it in a variable
 			var film = $('#term').val();
 			
-			//Check if the user has entered anything
-			if (film === '') {
+				//Check if the user has entered anything
+				if (film === '') {
 			
-				//If the input field was empty, display a message
-				$('#poster').html("<h2 class='loading'>Ha! We haven't forgotten to validate the form! Please enter something.</h2>");
+					//If the input field was empty, display a message
+					$('#poster').html("<h2 class='loading'>Ha! We haven't forgotten to validate the form! Please enter something.</h2>");
 				
-			} else {
+				} else {
 			
-				//They must have entered a value, carry on with API call, first display a loading message to notify the user of activity
-				$('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
+					//They must have entered a value, carry on with API call, first display a loading message to notify the user of activity
+					$('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
 				
-				//The original v2.1 API call
-				//$.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/fb565d74e9408068a827dcd87e0eb7d5/" + film + "?callback=?", function(json) {
+					//The original v2.1 API call
+					//$.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/fb565d74e9408068a827dcd87e0eb7d5/" + film + "?callback=?", function(json) {
 				
-				//TODO Use short form getJSON call instead of long-form AJAX call
-				//$.getJSON("http://api.themoviedb.org/3/search/movie?api_key=fb565d74e9408068a827dcd87e0eb7d5&language=en&query=" + film + "&callback=?", function(json) {
-				$.ajax({
-     				type: 'GET',
-      				url: 'http://api.themoviedb.org/3/search/movie?api_key=fb565d74e9408068a827dcd87e0eb7d5&query=' + film,
-      				async: false,
-      				jsonpCallback: 'test',
-      				contentType: 'application/json',
-      				dataType: 'jsonp',
-      				success: function(json) {
-
+					$.getJSON("http://api.themoviedb.org/3/search/movie?api_key=fb565d74e9408068a827dcd87e0eb7d5&language=en&query=" + film + "&callback=?", function(json) {
+				
 						//TMDb API v3 no longer provides the "Nothing found" message, so we need to change our if statement
 						if (json.results.length !== 0) {
 							console.log(json);
@@ -53,31 +44,16 @@ $(document).ready(function() {
 						
 							//The original v2.1 API call for when no results were found
 							//$.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/fb565d74e9408068a827dcd87e0eb7d5/goonies?callback=?", function(json) {
-					
-							$.ajax ({
-								type: 'GET',
-								url: 'http://api.themoviedb.org/3/search/movie?api_key=fb565d74e9408068a827dcd87e0eb7d5&query=goonies',
-								async: false,
-								jsonpCallback: 'test',
-								contentType: 'application/json',
-								dataType: 'jsonp',
-								success: function(json) {
+							$.getJSON("http://api.themoviedb.org/3/search/movie?api_key=fb565d74e9408068a827dcd87e0eb7d5&language=en&query=goonies&callback=?", function(json) {
+									console.log(json);
+
 									$('#poster').html('<h2 class="loading">We\'re afraid nothing was found for that search, we presume you were trying to search for The Goonies?</h2><img id="thePoster" src="http://cf2.imgobject.com/t/p/w500' + json.results[0].poster_path + '" />');
 									
 									//Here's the original v2.1 output code
-									//$('#poster').html('<h2 class="loading">We\'re afraid nothing was found for that search, we presume you were trying to search for The Goonies?</h2><img id="thePoster" src=' + json[0].posters[2].image.url + ' />');	
-									console.log(json);
-								},
-								error: function(e) {
-         							console.log(e.message);
-      							}
+									//$('#poster').html('<h2 class="loading">We\'re afraid nothing was found for that search, we presume you were trying to search for The Goonies?</h2><img id="thePoster" src=' + json[0].posters[2].image.url + ' />');
+
 							});
 						}
-        				
-      				},
-      				error: function(e) {
-         				console.log(e.message);
-      				}
 				});
 			}
 			return false;
